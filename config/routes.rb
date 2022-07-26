bafecf4e8d1f8f10ddc43b6803c2ca2e59069f79
Roberto Andres Condezo Monge <ra.condezomonge@gmail.com>
2022-07-26 17:17:02 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: :index do
-    resources :foods, except: [:update, :edit] do
+    resources :foods, except: [:update, :edit, :show] do
       resources :recipe_foods, except: [:update, :edit]
     end
     resources :recipes, except: [:update, :edit]
