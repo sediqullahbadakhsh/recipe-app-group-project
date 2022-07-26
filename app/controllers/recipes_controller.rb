d82ed31d7949bbfb2a0adb90dee5985ed9870d2e
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-07-26 22:31:39 +0430

    create recipe actions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,5 +5,24 @@ class RecipesController < ApplicationController
     def show
         @user = User.find(params[:user_id])
     end
-    
+
+    def new
+        @user = current_user
+        @recipe = Recipe.new
+    end
+
+    def create
+        @recipe = current_user.recipes.new(recipe_params)
+        @recipe.user_id = params[:user_id]
+        respond_to do |format|
+          if @recipe.save!
+            format.html do
+              redirect_to user_recipe_path(@recipe.user_id, @recipe.id), notice: 'Recipe created successfully!'
+            end
+          else
+            format.html { redirect_to new_user_recipes_path, alert: 'Failed to create recipe' }
+          end
+        end
+    end
+
 end
