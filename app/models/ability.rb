ffa0efcb4a2ccf2eee9bf419f07d72029c39b8c0
Roberto Andres Condezo Monge <ra.condezomonge@gmail.com>
2022-07-26 17:15:39 -0500

    Update ability file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,12 +2,12 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # guest user (not logged in)
+    # user ||= User.new # guest user (not logged in)
     if user.role == 'admin'
       can :manage, :all
     else
-      can :read, :all
-      can :manage, Food, id: user.id
+      can :read, Recipe, public: true
+      can :manage, Food, user_id: user.id
       can :manage, Recipe, user_id: user.id
     end
   end
