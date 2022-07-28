39242f8881aa9ca9fef119e75f221ddcedff2765
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-07-28 15:47:35 +0430

    create recipe details view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,6 +5,8 @@ class RecipesController < ApplicationController
 
   def show
     @user = User.find(params[:user_id])
+    @recipe = Recipe.find(params[:id])
+    @food = Food.all
   end
 
   def new
