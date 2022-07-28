237a11aa383c1e57e03041e4d775f2615b7cee84
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-07-28 23:12:55 +0430

    modify quantity datatype

--- db/migrate/20220725184807_create_recipe_foods.rb
+++ db/migrate/20220725184807_create_recipe_foods.rb
@@ -3,7 +3,7 @@
 class CreateRecipeFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :recipe_foods do |t|
-      t.string :quantity
+      t.integer :quantity
       t.belongs_to :food, index: true, foreign_key: true
       t.belongs_to :recipe, index: true, foreign_key: true
 
