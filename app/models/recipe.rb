82c8f4d2b8fa32c7936b57c14d7cd65f958ad9e8
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-07-29 20:08:00 +0430

    fix validations on recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,7 @@ class Recipe < ApplicationRecord
   has_many :recipe_foods, dependent: :delete_all
 
   validates :name, presence: true
+  validates :description, presence: true
   validates :preparation_time, presence: true
   validates :cooking_time, presence: true
 end
