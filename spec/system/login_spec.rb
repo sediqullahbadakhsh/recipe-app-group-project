a5904ff93070970a7bebab121b49dd23294d0380
sediqullahbadakhsh <sediqullah.badakhsh@gmail.com>
2022-07-29 20:08:24 +0430

    add unite test and views for login page and recipe modle

---
+++ spec/system/login_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+RSpec.describe 'Logins', type: :system do
+    before do
+        driven_by(:rack_test)
+    end
+    describe 'Login page' do
+        before :each do
+            visit user_session_path
+        end
+        it 'should have login text' do 
+            expect(page).to have_content 'Log in'
+        end
+        it 'should have input email' do 
+            expect(page).to have_content 'Email'
+        end
+        it 'should have password input' do 
+            expect(page).to have_content 'Password'
+        end
+        it 'should have login button' do 
+            expect(page).to have_button 'Log in'
+        end
+    end
+end
